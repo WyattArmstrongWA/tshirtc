516cc8d7c45dc1dd7e4e9f3f7f39b95568e98572
WyattArmstrongWA <wyattwaarmstrong@gmail.com>
2023-09-14 14:22:08 -0600

    motor buttons

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,37 +14,41 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+
+
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+
   WPI_TalonSRX m_leftMaster = new WPI_TalonSRX(3);
   WPI_TalonSRX m_rightMaster = new WPI_TalonSRX(13);
   WPI_TalonSRX m_rightSlave = new WPI_TalonSRX(2);
   WPI_TalonSRX m_leftSlave = new WPI_TalonSRX(1);
+  //assign drivetrain motor IDs
+
   CANSparkMax m_switchMotor = new CANSparkMax(8, MotorType.kBrushless);
+  //assign valve motor ID
+
   private final XboxController m_driverController = new XboxController(0);
+  //assign controller port
+
   DifferentialDrive drive = new DifferentialDrive(m_leftMaster, m_rightMaster);
+  //create differential drivetrain with leading/master motors
 
-  //private Compressor compressor = new Compressor();
-	//private Solenoid cannon = new Solenoid(Open_PORT_ID, Close_PORT_ID);
 
-  double startTime;
-  int x;
+  //double startTime;
+  //int x;
+  //create variables for use in timer and iteration logic
 
   @Override
   public void robotInit() {
+
     m_rightMaster.setInverted(true);
     m_rightSlave.setInverted(true);
+    //invert right side motors
+
     m_leftSlave.set(ControlMode.Follower, 3);
     m_rightSlave.set(ControlMode.Follower, 13);
+    //assign follower status to follower/slave motors with regards to leading/master motor IDs
+
   }
 
   @Override
@@ -62,22 +66,28 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    if(m_driverController.getAButtonPressed()) {
-      startTime = System.currentTimeMillis();
-      x++;
-    } else {
-      if(System.currentTimeMillis() - startTime < 0.5) {
-        if(x % 2 == 0) {
-          m_switchMotor.set(0.6);
-          } else {
-          m_switchMotor.set(-0.6);
-          }
-      } else {
-        m_switchMotor.set(0);
-      }
+    if(m_driverController.getXButtonPressed()) {
+      m_switchMotor.set(-0.8);
+      //When the X button is pressed, the motor runs in the reverse direction, closing? the cannon valve
     }
+    if(m_driverController.getXButtonReleased()) {
+      m_switchMotor.set(0);
+      //When the X button is released, the motor is stationary
+    }
+    
+
+    if(m_driverController.getBButtonPressed()) {
+      m_switchMotor.set(0.8);
+      //When the B button is pressed the motor runs in the forward direction, opening? the cannon valve
+    } 
+    if(m_driverController.getBButtonReleased()) {
+      m_switchMotor.set(0);
+      //When the B button is released, the motor is stationary
+    }
+
     drive.arcadeDrive(-m_driverController.getLeftY(), -m_driverController.getRightX());
-  }
+    //
+      }
 
   @Override
   public void disabledInit() {}
